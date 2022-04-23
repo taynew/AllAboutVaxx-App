edffdd30e039e9345522abfaed87038e9aa1984b
Abir Das <abirdasusa12@gmail.com>
2022-04-23 11:46:36 -0400

    Testing

--- AllAboutVaxxTest/clinicListViewController.swift
+++ AllAboutVaxxTest/clinicListViewController.swift
@@ -59,6 +59,6 @@ class clinicListViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        return 150
     }
 }
