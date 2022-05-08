8068954653b3ba704f71bf7e3a910e492d925fc9
Abir Das <abirdasusa12@gmail.com>
2022-05-08 12:40:16 -0400

    Added some about us

--- AllAboutVaxxTest/AboutUsViewController.swift
+++ AllAboutVaxxTest/AboutUsViewController.swift
@@ -11,14 +11,29 @@ class AboutUsViewController: UIViewController {
     
     
     @IBOutlet weak var abirImage: UIImageView!
+    @IBOutlet weak var derrickImage: UIImageView!
+    @IBOutlet weak var kanizImage: UIImageView!
+    @IBOutlet weak var haneulImage: UIImageView!
+    @IBOutlet weak var sanzidaImage: UIImageView!
+    @IBOutlet weak var pritomImage: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "About Us"
         abirImage.layer.cornerRadius = abirImage.frame.size.width / 2
         abirImage.clipsToBounds = true
+        derrickImage.layer.cornerRadius = derrickImage.frame.size.width / 2
+        derrickImage.clipsToBounds = true
+        kanizImage.layer.cornerRadius = kanizImage.frame.size.width / 2
+        kanizImage.clipsToBounds = true
+        haneulImage.layer.cornerRadius = haneulImage.frame.size.width / 2
+        haneulImage.clipsToBounds = true
+        sanzidaImage.layer.cornerRadius = sanzidaImage.frame.size.width / 2
+        sanzidaImage.clipsToBounds = true
+        pritomImage.layer.cornerRadius = pritomImage.frame.size.width / 2
+        pritomImage.clipsToBounds = true
     }
-    
 
     /*
     // MARK: - Navigation
